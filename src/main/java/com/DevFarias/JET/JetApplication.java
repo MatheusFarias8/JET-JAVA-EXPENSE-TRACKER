278b26a62b5eb653105ae7d8222f89cd472d80bf
Matheus Farias <matheusfarias3456@hotmail.com>
2026-06-06 12:47:20 -0300

    first commit

---
+++ src/main/java/com/DevFarias/JET/JetApplication.java
@@ -0,0 +1,13 @@
+package com.DevFarias.JET;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DevFarias/JET/JetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevFarias.JET;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
