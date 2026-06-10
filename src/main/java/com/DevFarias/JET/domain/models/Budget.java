f9185e831e1a68df513f3cf30138e3f0dba5a0b6
Matheus Farias <matheusfarias3456@hotmail.com>
2026-06-09 22:01:02 -0300

    setting up the Budget model

---
+++ src/main/java/com/DevFarias/JET/domain/models/Budget.java
@@ -0,0 +1,39 @@
+package com.DevFarias.JET.domain.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "TB_BUDGETS")
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false, updatable = false)
+    private int id;
+
+    @Column(nullable = false)
+    int month;
+
+    @Column(nullable = false)
+    int year;
+
+    @Column(nullable = false)
+    BigDecimal limitAmount;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+
+}
